0104efa8ee8adfa20ad193c13f838d630aea62f8
sabamokhlesi <59446430+sabamokhlesi@users.noreply.github.com>
2020-11-15 16:14:10 -0500

    Updates

---
+++ precache-manifest.5dcbeada0f0c37cb6b30799337ca1876.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f7b042f35a0e62e510e1d70eed351de",
+    "url": "/Finance-App/index.html"
+  },
+  {
+    "revision": "1857e7e95dc196d8f291",
+    "url": "/Finance-App/static/css/main.9d7176a1.chunk.css"
+  },
+  {
+    "revision": "b371a735282360ba619c",
+    "url": "/Finance-App/static/js/2.be890ab9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Finance-App/static/js/2.be890ab9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1857e7e95dc196d8f291",
+    "url": "/Finance-App/static/js/main.d343beff.chunk.js"
+  },
+  {
+    "revision": "73ab5b1d038dab6cbb76",
+    "url": "/Finance-App/static/js/runtime-main.ecfa98e6.js"
+  },
+  {
+    "revision": "2400a172dfc6c43f0f16ce875fd3cd68",
+    "url": "/Finance-App/static/media/1111.2400a172.jpg"
+  },
+  {
+    "revision": "4c40bf1756db4c2e396778f63578989d",
+    "url": "/Finance-App/static/media/2222.4c40bf17.jpg"
+  },
+  {
+    "revision": "2d301a1cb6772eea1c6d76176cf5f12e",
+    "url": "/Finance-App/static/media/3333.2d301a1c.jpg"
+  },
+  {
+    "revision": "2521deed9b0ada94a5d662a4dbbcb33e",
+    "url": "/Finance-App/static/media/dashboard-left.2521deed.png"
+  },
+  {
+    "revision": "4f92860cb48c982e5b8766bebb1286ea",
+    "url": "/Finance-App/static/media/editPage.4f92860c.png"
+  },
+  {
+    "revision": "31ea4d44678415abdd066f7ed27d68f2",
+    "url": "/Finance-App/static/media/features.31ea4d44.jpg"
+  },
+  {
+    "revision": "9da0fed1001ab5acc243433662c439a7",
+    "url": "/Finance-App/static/media/headerImg1.9da0fed1.png"
+  },
+  {
+    "revision": "ba3728623dd08b946f0eb22f1d14bc31",
+    "url": "/Finance-App/static/media/headerImg3.ba372862.png"
+  },
+  {
+    "revision": "44142d9426f01346869980bf13620aef",
+    "url": "/Finance-App/static/media/logo.44142d94.jpg"
+  },
+  {
+    "revision": "c7e363dde9dd87f3b858edceb8d43fa3",
+    "url": "/Finance-App/static/media/settings-bottom.c7e363dd.png"
+  },
+  {
+    "revision": "bc13cc0979fa90a93ff7172675d120ca",
+    "url": "/Finance-App/static/media/signInImg.bc13cc09.png"
+  },
+  {
+    "revision": "47383c7544cbb9638474fdbf031e0434",
+    "url": "/Finance-App/static/media/signUpImg.47383c75.gif"
+  },
+  {
+    "revision": "b556c4f4269ddc52e8040a1a8b58e94f",
+    "url": "/Finance-App/static/media/steps.b556c4f4.png"
+  }
+]);
